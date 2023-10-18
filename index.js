23c6047fe710beea3eeaf35adc6b3a065e5c17b0
Taylor Picard <taylorkpicard1998@gmail.com>
2023-10-18 17:38:55 -0500

    Exercise 4 & tests 6,7,8 passing

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ class Counter {
    * @param {number} initialNumber - the initial state of the count
    */
   constructor(initialNumber) {
-    let counter = initialNumber
+    this.count = initialNumber
   }
 
   /**
@@ -65,7 +65,10 @@ class Counter {
    * counter.countDown() // returns 0
    */
   countDown() {
-    // ✨ implement
+    if(this.count > 0){
+      return this.count--
+    }
+    return this.count
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -49,11 +49,19 @@ describe('[Exercise 4] Counter', () => {
     counter = new utils.Counter(3) // each test must start with a fresh couter
   })
   test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {
-    const initial = 3
-    expect(counter).toEqual(initial)
+    expect(counter.countDown()).toBe(3)
   })
-  // test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {})
-  // test('[8] the count eventually reaches zero but does not go below zero', () => {})
+  test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {
+    counter.countDown()
+    expect(counter.countDown()).toBe(2)
+  })
+  test('[8] the count eventually reaches zero but does not go below zero', () => {
+    counter.countDown()
+    counter.countDown()
+    counter.countDown()
+    counter.countDown()
+    expect(counter.countDown()).toBe(0)
+  })
 })
 
 describe('[Exercise 5] Seasons', () => {
